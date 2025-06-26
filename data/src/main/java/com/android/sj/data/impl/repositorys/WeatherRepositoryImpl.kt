9c7ab06f8c50a12068959eb77de151e9ba010eba
disuns <xyxo1992@naver.com>
2025-06-26 11:01:08 +0900

    refactor(channel-flow 롤백 작업중): mvi, mvvm코드 프로젝트 내부에서분기 및 event구조 명확히 하기위해 롤백 작업중

--- data/src/main/java/com/android/sj/data/impl/repositorys/WeatherRepositoryImpl.kt
+++ data/src/main/java/com/android/sj/data/impl/repositorys/WeatherRepositoryImpl.kt
@@ -11,25 +11,26 @@ import com.android.sj.domain.models.WeatherData
 import com.android.sj.domain.models.WeekRainSkyData
 import com.android.sj.domain.repositories.WeatherRepository
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class WeatherRepositoryImpl @Inject constructor(
     private val weatherDataSource: WeatherDataSource,
     private val mapper : WeatherDataMapper
 ) : WeatherRepository {
-    override fun fetchWeather(date: String, time: String, lat: String, lon: String): Channel<ApiResult<WeatherData>> {
+    override fun fetchWeather(date: String, time: String, lat: String, lon: String): Flow<ApiResult<WeatherData>> {
         val request = WeatherRequest(baseDate = date, baseTime = time, nx = lat, ny = lon)
 
         return mapper.responseToDomainWeather(weatherDataSource.fetchWeather(request.toMap()))
     }
 
-    override fun fetchTimeWeather(date: String, time: String, lat: String, lon: String): Channel<ApiResult<TimeWeatherData>> {
+    override fun fetchTimeWeather(date: String, time: String, lat: String, lon: String): Flow<ApiResult<TimeWeatherData>> {
         val request = WeatherRequest(baseDate = date, baseTime = time, nx = lat, ny = lon)
 
         return  mapper.responseToDomainTimeWeather(weatherDataSource.fetchTimeWeather(request.toMap()))
     }
 
-    override fun fetchWeekRainSky(landCode: String, time: String): Channel<ApiResult<WeekRainSkyData>> {
+    override fun fetchWeekRainSky(landCode: String, time: String): Flow<ApiResult<WeekRainSkyData>> {
         val request = WeekRainSkyRequest(regId = landCode, tmFc = time)
 
         return mapper.responseToDomainRainSky(weatherDataSource.fetchWeekRainSky(request.toMap()))
--- domain/src/main/java/com/android/sj/domain/usecase/usecaseinterface/weather/GetTimeWeatherUseCase.kt
+++ domain/src/main/java/com/android/sj/domain/usecase/usecaseinterface/weather/GetTimeWeatherUseCase.kt
@@ -2,8 +2,8 @@ package com.android.sj.domain.usecase.usecaseinterface.weather
 
 import com.android.sj.domain.ApiResult
 import com.android.sj.domain.models.TimeWeatherData
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 
 interface GetTimeWeatherUseCase {
-    operator fun invoke(date: String, time: String, lat: String, lon: String) : Channel<ApiResult<TimeWeatherData>>
+    operator fun invoke(date: String, time: String, lat: String, lon: String) : Flow<ApiResult<TimeWeatherData>>
 }
